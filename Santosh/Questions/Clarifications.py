7b09a5eb22fe7c9be0c76cd4054a3ed7891dc2bf
Santu Rach <santu.rach@gmail.com>
2024-08-13 20:58:58 +0530

    watching videos Offline

--- Santosh/Questions/Clarifications.py
+++ Santosh/Questions/Clarifications.py
@@ -7,10 +7,24 @@
 
 
 # Unsolved programs
-# Exercise 8
+
 # Python list - program to print a combination of 2 elements from the list whose sum is 10.
 # Python list - program to generate all sublists with 5 or more elements in it from the given list.
 # Python program to combine two list elements as a sublist in a list.
 # program to iterate over two lists simultaneously and create a list of sublists.
 # program to remove consecutive duplicates of given lists.
-# program to pack consecutive duplicates of given list elements into sublists..
+# program to pack consecutive duplicates of given list elements into sublists.
+# program to select random numbers from the list.
+# program to select random numbers from the list.
+# Python program to zip two lists of lists into a list.
+# to find maximum and minimum values in the given heterogeneous list.
+# program to sort a given list in ascending order according to the sum of its sublist.
+# program to create a dictionary with two lists.
+# program to replace ‘Java’ with ‘Python’ from the given list.
+# Python Program to get the Median of all the elements from the list.
+# Python Program to get the Standard deviation of the list element.
+# program to convert all numbers to binary format from a given list.
+# program to convert all the numbers into Roman numbers from the given list.
+# .
+
+
